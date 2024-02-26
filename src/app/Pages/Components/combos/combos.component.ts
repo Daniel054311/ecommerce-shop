4facdfc691074a55be46761df766d58317f6b9a7
Daniel Forson <danielkojoforson27@gmail.com>
2024-02-26 15:57:48 +0100

    added the men and the women component to the route

---
+++ src/app/Pages/Components/combos/combos.component.ts
@@ -0,0 +1,47 @@
+import { Component } from '@angular/core';
+import { ProductService } from '../../Service/Product/product.service';
+import { Product } from '../../Service/Product/product-data';
+import { EMPTY, catchError, finalize } from 'rxjs';
+import { CommonModule } from '@angular/common';
+import { FooterComponent } from "../../Shared/footer/footer.component";
+import { LimelightComponent } from "../../Shared/limelight/limelight.component";
+
+@Component({
+    selector: 'app-combos',
+    standalone: true,
+    templateUrl: './combos.component.html',
+    styleUrl: './combos.component.css',
+    imports: [CommonModule, FooterComponent, LimelightComponent]
+})
+export class CombosComponent {
+  products: Product[] = [];
+  loading: boolean = false;
+
+  constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.getLimelight()
+    }
+
+  getLimelight(): void {
+    this.loading = true;
+    this.productService.getProducts()
+      .pipe(
+        catchError(() => EMPTY),
+        finalize(() => this.loading = false)
+      )
+      .subscribe(products => {
+        this.products = products.filter(product => product.title.toLowerCase().includes("sneaker"));
+        console.log(products)
+      });
+  }
+
+  getFavourite(product: Product): void {
+    // Toggle the liked property of the product
+    product.liked = !product.liked;
+  }
+  trackFavourite(index: number, product: any): number {
+    return product.id;
+  }
+
+}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './Pages/Components/home/home.component';
+import { WomenComponent } from './Pages/Components/women/women.component';
+import { MenComponent } from './Pages/Components/men/men.component';
+import { CombosComponent } from './Pages/Components/combos/combos.component';
 
 export const routes: Routes = [
-  {path:'',component:HomeComponent}
+  { path: '', component: HomeComponent },
+  { path: 'women', component: WomenComponent },
+  { path: 'men', component: MenComponent },
+  {path:'combos',component:CombosComponent}
 ];
